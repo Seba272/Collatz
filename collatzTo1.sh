5e7102b3793efcede538490d6e28282475a01728
Seba272 <git@sebang.fastmail.fm>
2020-09-20 11:39:47 +0200

    Correct the names in the bash script.

--- collatzTo1.sh
+++ collatzTo1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "How many?"
 read MAX
-python prova02.py $MAX
-dot -Tpng graph02-"$MAX".gv -o graph02-"$MAX".png
-Open graph02-"$MAX".png
+python collatzTo1.py $MAX
+dot -Tpng collatzTo1-"$MAX".gv -o collatzTo1-"$MAX".png
+Open collatzTo1-"$MAX".png
