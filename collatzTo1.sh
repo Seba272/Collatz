e06dc712ff95463345d6f35a4f43470263124855
Seba272 <git@sebang.fastmail.fm>
2020-09-20 12:10:20 +0200

    Make the bash script more portable and more informative.

--- collatzTo1.sh
+++ collatzTo1.sh
@@ -3,4 +3,8 @@ echo "How many?"
 read MAX
 python collatzTo1.py $MAX
 dot -Tpng collatzTo1-"$MAX".gv -o collatzTo1-"$MAX".png
-Open collatzTo1-"$MAX".png
+echo "DOT representation written in"
+echo "./collatzTo1-"$MAX".gv"
+echo "PNG picture written in " 
+echo "./collatzTo1-"$MAX".png"
+
